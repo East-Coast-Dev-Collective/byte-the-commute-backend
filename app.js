1fe77282c55dcbd16b5242cab46eacc7e2c9ae44
Michael Rhoads <mrhoads14@outlook.com>
2026-03-01 16:41:32 -0500

    add middleware invocations for json parsing and cors and add a health check GET endpoint

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" }));
+
+app.get("/api/health", (req, res) => {
+  res.send("Server healthy!")
+});
+
+
+
+
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Something has gone wrong on the backend server side");
+});
 
 export default app;
