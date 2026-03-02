cd5732b7c68d7b6701fd444a6719d2eeb21c3724
Michael Rhoads <mrhoads14@outlook.com>
2026-03-01 21:06:44 -0500

    add middleware to send a 404 for unknown routes and 500 for final error handling

--- app.js
+++ app.js
@@ -7,16 +7,23 @@ app.use(express.json());
 app.use(cors({ origin: "http://localhost:5173" }));
 
 app.get("/api/health", (req, res) => {
-  res.send("Server healthy!")
+  res.json({ status: "success", message: "Server healthy!" });
 });
 
 
+// matches all routes that have not yet been matched -- catch-all
+// should come after all other routes, but before error handling
+app.use((req, res, next) => {
+  res.status(404).json({ status: "error", message: "Not Found Error: Unknown endpoint" });
+});
 
-
-
+// error handling
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status(500).send("Something has gone wrong on the backend server side");
+  res.status(500).json({
+    status: "error",
+    message: "Something has gone wrong on the backend server"
+  });
 });
 
 export default app;
